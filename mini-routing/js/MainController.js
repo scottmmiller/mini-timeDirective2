b509d14fff17218a9ce960a2be405ac8b30f8064
Scott M Miller <smmiller.work@gmail.com>
2015-05-20 19:06:16 -0600

    step 1 complete

---
+++ mini-routing/js/MainController.js
@@ -0,0 +1,7 @@
+var app = angular.module('timeApp');
+
+app.controller('MainController', function($scope) {
+
+
+
+});
---
+++ mini-routing/js/timeDirective.js
@@ -0,0 +1,7 @@
+var app = angular.module('timeApp');
+
+app.directive('showTime', function() {
+
+
+
+});
